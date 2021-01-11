88504125c5b6ac73bb2b58bd9bb1bcced1c23861
isaiah3031 <isaiah.whitfield@gmail.com>
2021-01-11 12:15:50 -0500

    finished user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,40 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
-  validates :username, presence: true
+  validates :username, :password_digest, :session_token, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true, message: 'is too short (minimum is 6 characters)'}
+  before_validation :ensure_session_token
+
+  attr_accessor :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      false
+    end
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+  end
+
+  def ensure_session_token
+    self.session_token || self.session_token = User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(self.password_digest)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,10 @@ require 'spec_helper'
 RSpec.describe User, type: :model do
   context 'validations' do
     it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_length_of(:password)
+          .is_at_least(6)
+          .allow_nil
+       }       
   end
 end

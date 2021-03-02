d3aac7803e4fde3f0b0d03c8d6f8d7a467b26439
isaiah3031 <isaiah.whitfield@gmail.com>
2021-03-02 10:29:24 -0500

    Changed backend api urls to match new heroku url.

--- client/src/frontend/util/cart_api_util.js
+++ client/src/frontend/util/cart_api_util.js
@@ -3,12 +3,12 @@ import $ from 'jquery'
 export const fetchUserCart = (userId) => 
   $.ajax({
     type: 'GET',
-    url: `https://estore-iw-backend.herokuapp.com/api/users/${userId}/carts/1`
+    url: `https://thawing-falls-95338.herokuapp.com/api/users/${userId}/carts/1`
   })
 
 export const saveUserCart = (userId, cart) =>
   $.ajax({
     type: 'PUT',
-    url: `https://estore-iw-backend.herokuapp.com/api/users/${userId}/carts/1`,
+    url: `https://thawing-falls-95338.herokuapp.com/api/users/${userId}/carts/1`,
     data: {cart: {ordered: false, owner_id: userId, contents: cart}}
   })
--- client/src/frontend/util/session_api_util.js
+++ client/src/frontend/util/session_api_util.js
@@ -3,19 +3,19 @@ import $ from "jquery"
 export const signup = (user) => 
   $.ajax({
     type: 'POST',
-    url: 'https://estore-iw-backend.herokuapp.com/api/users',
+    url: 'https://thawing-falls-95338.herokuapp.com/api/users',
     data: user
   })
 
 export const login = (user) => 
   $.ajax({
     type: 'POST',
-    url: 'https://estore-iw-backend.herokuapp.com/api/sessions',
+    url: 'https://thawing-falls-95338.herokuapp.com/api/sessions',
     data: user
   })
 
 export const logout = () => 
   $.ajax({
     type: 'DELETE',
-    url: 'https://estore-iw-backend.herokuapp.com/api/sessions'
+    url: 'https://thawing-falls-95338.herokuapp.com/api/sessions'
   })
